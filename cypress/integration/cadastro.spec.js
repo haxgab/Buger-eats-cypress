9929d43497275e39b837a1565eb7832afcc6a7c6
haxgab <gabriel.e.richter1996@gmail.com>
2022-03-28 17:13:33 -0300

    Testes de Cypress

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,71 @@
+import signup from '../pages/SignupPage'
+// objeto vindo com o import acima
+import signupFactory from '../factories/SignupFactory'
+describe('cadastro', () => {
+
+  /*  beforeEach(function(){
+      cy.fixture('deliver').then((d)=>{
+        this.deliver = d;
+      }) 
+    })*/
+
+    it('usuario deve se tornar um entregador', function() {
+
+        var deliver =  signupFactory.deliver()
+
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+    })
+
+    it('cpf incorreto', function() {
+
+        var deliver =  signupFactory.deliver()
+        deliver.cpf = '097474569aa'
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+    })
+
+    
+    it('cpf Email incorreto', function() {
+
+        var deliver =  signupFactory.deliver()
+        deliver.email = 'gabriel.com.br'
+
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! Email com formato inválido.')
+    })
+
+    context('Campos obrigatorios', function(){
+        const  messages = [
+            {field: 'name', output: 'É necessário informar o nome'},
+            {field: 'cpf', output: 'É necessário informar o CPF'},
+            {field: 'email', output: 'É necessário informar o email'},
+            {field: 'postalcode', output: 'É necessário informar o CEP'},
+            {field: 'number', output: 'É necessário informar o número do endereço'},
+            {field: 'deliver_method', output: 'Selecione o método de entrega'},
+            {field: 'cnh', output: 'Adicione uma foto da sua CNH'}
+
+        ]
+        before(function(){
+            signup.go()
+            signup.submit()
+        })
+        messages.forEach(function(msg){
+            it(`${msg.field} é obrigatorio`, function(){
+                signup.alertMessageShouldBe(msg.output)
+            })
+        })
+    })
+
+
+})
